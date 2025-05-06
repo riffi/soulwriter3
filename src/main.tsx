d480ddf524f5e6a32bc5dea60ab22e6eb5b4a9dc
Belenko Vladimir <vbelenko@ntdev.ru>
2025-05-06 16:51:14 +0300

    [main] Приложение превращено в pwa

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,13 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { registerSW } from 'virtual:pwa-register'
+if ('serviceWorker' in navigator) {
+  registerSW({
+    immediate: true,
+    onRegisteredSW(swUrl, registration) {
+      console.log('Service Worker registered:', registration)
+    }
+  })
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,38 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import {VitePWA} from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'IncLumin',
+        short_name: 'IncLumin',
+        description: 'Приложение для писателя',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      }
+    })
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
