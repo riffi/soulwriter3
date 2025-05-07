2fe965d58f9b9fb649be58983eace4dea5b565de
Belenko Vladimir <vbelenko@ntdev.ru>
2025-05-07 10:23:15 +0300

    [main] доработана страница редактирования заметки

--- src/components/notes/NoteEditPage/NoteEditPage.tsx
+++ src/components/notes/NoteEditPage/NoteEditPage.tsx
@@ -1,9 +1,13 @@
-import { useEffect, useState } from "react";
+import {useCallback, useEffect, useState} from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import {TextInput, Button, Container, Group, LoadingOverlay, TagsInput, Paper} from "@mantine/core";
+import {TextInput, Button, Container, Group, LoadingOverlay, TagsInput, Paper, Drawer} from "@mantine/core";
 import { RichEditor } from "@/components/shared/RichEditor/RichEditor";
 import { configDatabase } from "@/entities/configuratorDb";
 import { notifications } from '@mantine/notifications';
+import {useMedia} from "@/providers/MediaQueryProvider/MediaQueryProvider";
+import { IconMenu2 } from "@tabler/icons-react";
+import {InlineEdit} from "@/components/shared/InlineEdit/InlineEdit";
+import {INote} from "@/entities/BookEntities";
 
 export const NoteEditPage = () => {
   const { uuid } = useParams();
@@ -11,6 +15,8 @@ export const NoteEditPage = () => {
   const [note, setNote] = useState<INote | null>(null);
   const [loading, setLoading] = useState(true);
   const [tags, setTags] = useState<string[]>([]);
+  const {isMobile} = useMedia();
+  const [drawerOpened, setDrawerOpened] = useState(false);
 
   useEffect(() => {
     const loadNote = async () => {
@@ -24,35 +30,37 @@ export const NoteEditPage = () => {
     loadNote();
   }, [uuid]);
 
-  const handleSave = async () => {
-    if (!note) return;
+  const handleSave = async (data: INote) => {
+    if (!data) return;
 
-    await configDatabase.notes.update(note.id!, {
-      ...note,
+    await configDatabase.notes.update(data.id!, {
+      ...data,
       tags: tags.join(',') // Преобразуем массив в строку
     });
-
-    notifications.show({
-      title: 'Сохранено',
-      message: 'Изменения успешно сохранены',
-      color: 'green'
-    });
+    setNote(data);
   };
+
+  const handleContentChange = useCallback((content: string) => {
+    const updated = { ...note!, body: content };
+    setNote(updated);
+    configDatabase.notes.update(updated.id!, updated);
+  }, [note]);
+
 
   if (loading) return <LoadingOverlay visible />;
 
-  return (
-      <Container size="xl" p="0">
-        <Paper p={"md"}>
+  const headerContent = (
+      <>
         <Button variant="subtle" onClick={() => navigate(-1)}>
           ← Назад к списку
         </Button>
 
-        <TextInput
+        <InlineEdit
+            value={note?.title}
             label="Название заметки"
-            value={note?.title || ''}
-            onChange={(e) => setNote({ ...note!, title: e.target.value })}
-            mb="md"
+            onChange={async (value) => {
+              await handleSave({ ...note!, title: value })
+            }}
         />
 
         <TagsInput
@@ -61,20 +69,49 @@ export const NoteEditPage = () => {
             onChange={setTags}
             mb="md"
         />
+      </>
+  );
+  return (
+      <Container size="xl" p="0">
+        <Paper p={"md"}>
+          {isMobile ? (
+              <>
+                <Group justify="space-between">
+                  <Button leftSection={<IconMenu2 size={18} />} onClick={() => setDrawerOpened(true)}>
+                    Меню
+                  </Button>
+                  <Button onClick={handleSave}>Сохранить</Button>
+                </Group>
 
-        <RichEditor
-            initialContent={note?.body}
-            mobileConstraints={
-              {top: 50, bottom: 0}
-            }
-            onContentChange={(content) => setNote({ ...note!, body: content })}
-        />
+                <Drawer
+                    opened={drawerOpened}
+                    onClose={() => setDrawerOpened(false)}
+                    title="Редактирование"
+                    position="left"
+                    size="100%"
+                >
+                  {headerContent}
+                </Drawer>
+              </>
+          ) : (
+              headerContent
+          )}
 
-        <Group justify="flex-end" mt="md">
-          <Button onClick={handleSave}>
-            Сохранить
-          </Button>
-        </Group>
+          <RichEditor
+              initialContent={note?.body}
+              mobileConstraints={
+                {top: 120, bottom: 0}
+              }
+              onContentChange={handleContentChange}
+          />
+
+          {!isMobile && (
+              <Group justify="flex-end" mt="md">
+                <Button onClick={handleSave}>
+                  Сохранить
+                </Button>
+              </Group>
+          )}
         </Paper>
       </Container>
   );
